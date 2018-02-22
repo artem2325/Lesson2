10d95d18673557dd4b04b16c04a8f48f3058530c
Artem <artem2325@bk.ru>
2018-02-22 03:11:30 +0300

    Numbers exercise fixed

---
+++ Numbers.java
@@ -0,0 +1,10 @@
+public class Numbers {
+    public static void main(String[] Args) {
+        int newNumber = 54556;
+        System.out.println("Введенное число: " + newNumber);
+        if (newNumber % 10 == 7) {
+            System.out.println("Последнее число семь");
+        } else
+            System.out.println("Последнее число не является семеркой");
+    }
+}
